1df2894a44d62b3f3d5028f034a0147856bd004f
Murod <murodjon.umarovv@gmail.com>
2022-05-22 12:31:14 +0500

    *

---
+++ script/script.js
@@ -0,0 +1,56 @@
+window.addEventListener('DOMContentLoaded', function () {
+    window.addEventListener('scroll', function () {
+        const header = this.document.querySelector('header');
+        header.classList.toggle('sticky', window.scrollY > 0)
+    })
+
+    const menuBtn = document.querySelector('.menu-btn')
+    const navigation = document.querySelector('.navigation')
+    const navigationItems = document.querySelectorAll('.navigation a')
+
+    menuBtn.addEventListener('click', () => {
+        menuBtn.classList.toggle('active')
+        navigation.classList.toggle('active')
+    })
+
+    navigationItems.forEach(navItem => {
+        navItem.addEventListener('click', () => {
+            menuBtn.classList.remove('active')
+            navigation.classList.remove('active')
+        })
+    })
+
+    const scrollBtn = document.querySelector('.scrollToTop-btn')
+    window.addEventListener('scroll', () => {
+        scrollBtn.classList.toggle('active', window.scrollY > 500)
+    })
+    scrollBtn.addEventListener('click', () => {
+        document.body.scrollTop = 0;
+        document.documentElement.scrollTop = 0;
+    })
+
+    window.addEventListener('scroll', () => {
+        let reveals = document.querySelectorAll('.reveal')
+
+        for (let i = 0; i < reveals.length; i++) {
+            let windowHeight = window.innerHeight;
+            let revealTop = reveals[i].getBoundingClientRect().top;
+            let revealPoint = 50;
+
+            if (revealTop < windowHeight - revealPoint) {
+                reveals[i].classList.add('active')
+            }
+        }
+    })
+})
+
+
+function hideLoader() {
+    document.getElementById('Loading').style.display = 'none';
+}
+    
+    // jQuery version of hideLoader()
+
+// $(window).on('load', function(){
+//     $('#Loading').fadeOut('slow');
+// })
